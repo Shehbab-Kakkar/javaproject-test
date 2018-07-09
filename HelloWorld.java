969b9912fb1169a1e84343e2df78e776e329c166
root <root@centos7.se2.ipsoft.com>
2018-07-09 11:16:46 +0200

    updates

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("AUTO COMMIT");
+        System.out.println("AUTO COMMIT>>>>>>>>>>");
     }
 }
